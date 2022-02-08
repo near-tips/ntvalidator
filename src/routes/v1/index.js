5aab0c444919bee5e2e3f9d5088291d2862548c3
hlib <kiskesis@gmail.com>
2022-02-08 22:56:29 +0200

    Added new encrypt scheme

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -3,9 +3,42 @@ const fs = require("fs");
 const { KeyPairEd25519 } = require("near-api-js/lib/utils");
 const axios = require("axios");
 const Base58 = require("base-58");
+const borsh = require("borsh");
 const { stackKey } = require('../../config/vars');
+const BigInteger = require('big-integer');
 
 const router = express.Router();
+
+const Service = {
+    Stackoverflow: 0,
+    Twitter: 1,
+    Telegram: 2
+}
+
+class ServiceId {
+    constructor({ service, id }) {
+        this.service = service
+        this.id = id
+    }
+}
+
+const zero = BigInteger(0);
+const n256 = BigInteger(256);
+
+function toLittleEndian(bigNumber) {
+    let result = new Uint8Array(8);
+    let i = 0;
+    while (bigNumber.greater(zero)) {
+        result[i] = bigNumber.mod(n256);
+        bigNumber = bigNumber.divide(n256);
+        i += 1;
+    }
+    return result;
+}
+
+function toBigEndian(bytes) {
+    return toLittleEndian(bytes).reverse();
+}
 
 router.post('/trans/sign', async (req, res) => {
     try {
@@ -16,11 +49,35 @@ router.post('/trans/sign', async (req, res) => {
         const { privateKey } = JSON.parse(fileData.toString())
         const keyPair = new KeyPairEd25519(privateKey)
 
-        // Create and encrypt message
-        const msg = userId + accessToken;
-        const unit8Message = new Uint8Array(Buffer.from(msg))
-        const signature = Buffer.from(keyPair.sign(unit8Message).signature).toString('hex');
-        // Encode validator public key
+        // Struct serialization
+        const value = new ServiceId({ service: Service.Stackoverflow, id: userId })
+        const schema = new Map([
+            [ServiceId,
+                { kind: 'struct', fields: [['service', 'u8'], ['id', 'string']] }]
+        ]);
+        const bufferSerializedMessage = borsh.serialize(schema, value);
+
+        // Convert current date to Uint8Array
+        const date = Math.floor(new Date() / 1000)
+        const bigInt = new BigInteger(date)
+        const dateUint8Array = toBigEndian(bigInt);
+
+        // Convert all message elements to Uint8Array
+        const array1 = new Uint8Array(Buffer.from(accessToken))
+        const array2 = new Uint8Array(bufferSerializedMessage)
+        const array3 = new Uint8Array(Buffer.from(userId))
+        const array4 = dateUint8Array
+
+        // Merge all message elements into one Uint8Array
+        const mergedArray = new Uint8Array(array1.length + array2.length + array3.length + array4.length);
+        mergedArray.set(array1);
+        mergedArray.set(array2, array1.length);
+        mergedArray.set(array3, array1.length + array2.length);
+        mergedArray.set(array4, array1.length + array2.length + array3.length);
+
+        // Sign message with KeyPair
+        const signature = Buffer.from(keyPair.sign(mergedArray).signature).toString('hex');
+        // Encode keyPair publicKey with Base58
         const validatorId = Base58.encode(keyPair.getPublicKey().data)
 
         // Get user data

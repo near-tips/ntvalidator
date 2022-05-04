8c84be2c3d03fc2b6bbeb45d1fb32adda9ea4975
Verkhohliad <ihor.verkhohliad@gmail.com>
2022-05-05 00:29:51 +0300

    WIP: WORKED SIGNATURE!

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -5,16 +5,28 @@ const axios = require("axios");
 const Base58 = require("base-58");
 const borsh = require("borsh");
 const { stackKey } = require('../../config/vars');
-const BigInteger = require('big-integer');
 const crypto = require('crypto');
-
 
 const router = express.Router();
 
+const fromHexToU8Array = hexString =>
+  new Uint8Array(hexString.match(/.{1,2}/g).map(byte => parseInt(byte, 16)));
+
+const toHexString = bytes =>
+  bytes.reduce((str, byte) => str + byte.toString(16).padStart(2, '0'), '');
+
 const Service = {
-    Stackoverflow: 0,
+    Stackoverflow: 'Stackoverflow',
     Twitter: 1,
     Telegram: 2
+}
+
+class ValidatorMsg {
+    constructor({ service_id, account_id, deadline }) {
+        this.service_id = service_id
+        this.account_id = account_id
+        this.deadline = deadline
+    }
 }
 
 class ServiceId {
@@ -24,71 +36,81 @@ class ServiceId {
     }
 }
 
-const zero = BigInteger(0);
-const n256 = BigInteger(256);
-
-function toLittleEndian(bigNumber) {
-    let result = new Uint8Array(8);
-    let i = 0;
-    while (bigNumber.greater(zero)) {
-        result[i] = bigNumber.mod(n256);
-        bigNumber = bigNumber.divide(n256);
-        i += 1;
-    }
-    return result;
-}
-
-function toBigEndian(bytes) {
-    return toLittleEndian(bytes).reverse();
-}
-
 router.post('/trans/sign', async (req, res) => {
     try {
-        const { accessToken, userId } = req.body;
+        const { accessToken, userId, accountId } = req.body;
+        console.log({ accountId, userId })
+        // const userId = '17694405'
 
         // Get validator keys
         const fileData = fs.readFileSync('./generatedKeys.js')
         const { privateKey } = JSON.parse(fileData.toString())
         const keyPair = new KeyPairEd25519(privateKey)
 
-        // Struct serialization
-        const value = new ServiceId({ service: Service.Stackoverflow, id: userId })
+        const deadline = (new Date().getTime() + 1200 * 60000) * Math.pow(10, 6)
+        // const deadline = 1651531536353000000
+
+        // Value for serialization
+        const value = new ValidatorMsg({
+            service_id: new ServiceId({ service: Service.Stackoverflow, id: userId }),
+            account_id: accountId,
+            deadline: String(deadline),
+        })
+
+        // Schema for serialization
         const schema = new Map([
-            [ServiceId,
-                { kind: 'struct', fields: [['service', 'u8'], ['id', 'string']] }]
+            [ValidatorMsg, {
+                kind: 'struct',
+                fields: [
+                    ['service_id', ServiceId], ['account_id', 'string'], ['deadline', 'u64']
+                ]
+            }],
+            [ServiceId, {
+                kind: 'struct',
+                fields: [
+                    ['service', 'u8'], ['id', 'string']
+                ]
+            }]
         ]);
-        const bufferSerializedMessage = borsh.serialize(schema, value);
+        const buffer = borsh.serialize(schema, value);
+        const u8 = new Uint8Array(buffer)
 
-        // Convert current date to Uint8Array
-        const date = (new Date().getTime() + 3 * 60000) * Math.pow(10, 6)
-        const bigInt = new BigInteger(date)
-        const dateUint8Array = toBigEndian(bigInt);
+        const des = borsh.deserialize(schema, ValidatorMsg, buffer)
 
-        //Name of the file : sha512-hash.js
-        //creating hash object
-        const hash = crypto.createHash('sha512');
-        //passing the data to be hashed
-        const hashedAccessToken = hash.update(accessToken);
-        //Creating the hash in the required format
-        const hexedAccessToken = hashedAccessToken.digest('hex');
+        const des2 = borsh.deserialize(schema, ValidatorMsg, Buffer.from([
+            0,   8,   0,   0,   0,  49, 55,  54,  57,  52,
+            52,  48,  53,  19,   0,   0,  0, 118, 101, 114,
+            107, 104, 111, 104, 108, 105, 97, 100,  46, 116,
+            101, 115, 116, 110, 101, 116, 64, 250, 172, 159,
+            46, 107, 235,  22
+        ]))
 
-        // Convert all message elements to Uint8Array
-        const array1 = new Uint8Array(Buffer.from(hexedAccessToken))
-        const array2 = new Uint8Array(bufferSerializedMessage)
-        const array3 = new Uint8Array(Buffer.from(userId))
-        const array4 = dateUint8Array
+        console.log({ buffer, u8, des, des2 })
+        console.log({ value })
 
-        // Merge all message elements into one Uint8Array
-        const mergedArray = new Uint8Array(array1.length + array2.length + array3.length + array4.length);
-        mergedArray.set(array1);
-        mergedArray.set(array2, array1.length);
-        mergedArray.set(array3, array1.length + array2.length);
-        mergedArray.set(array4, array1.length + array2.length + array3.length);
+        // Hashing serialized data
+        const sha512 = crypto.createHash('sha512');
+        const hash = sha512.update(buffer);
+        const hex = hash.digest('hex');
 
-        // Sign message with KeyPair
-        const signature = Buffer.from(keyPair.sign(mergedArray).signature).toString('hex');
+        // Format from hex to U8Array
+        const message = fromHexToU8Array(hex)
+
+        console.log({ message, hex })
+
+        // Sign with the validator key
+        // const sign = keyPair.sign(message).signature;
+
+        // const signature = toHexString(keyPair.sign(message).signature)
+        const signatureT = toHexString(keyPair.sign(u8).signature)
+        // const signature = keyPair.sign(u8).signature
+        const signature = keyPair.sign(u8).signature;
+        const signatureHex = Buffer.from(signature).toString('hex');
+        // const sig = Buffer.from(keyPair.sign(message).signature).toString('hex');
         // Encode keyPair publicKey with Base58
         const validatorId = Base58.encode(keyPair.getPublicKey().data)
+
+        console.log({ signatureT, signature, signatureHex, validatorId })
 
         // Get user data
         const { data } = await axios.get('https://api.stackexchange.com/2.3/me', {
@@ -102,14 +124,14 @@ router.post('/trans/sign', async (req, res) => {
         // Validate that user id same that we get by that access_token
         if (data.items[0].user_id.toString() === userId) {
             res.json({
-                signature,
+                signature: signature,
                 validatorId,
-                deadline: date,
+                deadline: String(deadline),
             });
         } else {
-            res.status(400).json({
+            res.status(401).json({
                 success: false,
-                message: 'Can not validate'
+                message: 'Unauthorized'
             });
         }
     } catch (error) {

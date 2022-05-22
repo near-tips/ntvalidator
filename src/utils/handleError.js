40340dc2b48081360310cd757753d638b6b006e8
hlib <kiskesis@gmail.com>
2022-05-22 15:28:51 +0300

    Add environment

--- src/utils/handleError.js
+++ src/utils/handleError.js
@@ -1,11 +1,12 @@
 const axios = require("axios");
-const { errorBotServer, service } = require('../config/vars');
+const { errorBotServer, env, service } = require('../config/vars');
 
 const handleError = async (error, funcName) => {
     await axios.post(errorBotServer + '/v1/handleError', {
         service,
         error: JSON.stringify(error),
         funcName,
+        environment: env
     }).catch((e) => console.log('Cant handle error', e.response.data.details.body))
 }
 
